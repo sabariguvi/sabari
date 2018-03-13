25a930dbcf252ca946b74181529118670d27e7f9
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-03-13 16:23:57 +0530

    Create sab115.c

---
+++ sab115.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n[5],a,i;
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d",&n[i]);
+	}
+	printf("\nenter the num");
+	scanf("%d",&a);
+            for(i=1;i<=5;i++)
+            {
+            printf("\n%d",n[a]);
+            break;
+		
+            }
+	
+	
+	
+	return 0;
+}
